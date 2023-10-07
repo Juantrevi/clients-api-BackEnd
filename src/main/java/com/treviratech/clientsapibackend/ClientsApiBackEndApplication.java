65b41ceb7ec99e4655f31c1c996af5920b5dd442
Juantrevi <juantrevi70@gmail.com>
2023-10-07 13:56:40 +0100

    Initial commit

---
+++ src/main/java/com/treviratech/clientsapibackend/ClientsApiBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.treviratech.clientsapibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientsApiBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientsApiBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/treviratech/clientsapibackend/ClientsApiBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treviratech.clientsapibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsApiBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
