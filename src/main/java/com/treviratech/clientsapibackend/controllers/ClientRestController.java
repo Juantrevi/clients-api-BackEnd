a04412e0c255f9f82443125c921b88b3f0ee176d
Juantrevi <juantrevi70@gmail.com>
2023-10-08 11:24:11 +0100

    @Crossorigin

--- src/main/java/com/treviratech/clientsapibackend/controllers/ClientRestController.java
+++ src/main/java/com/treviratech/clientsapibackend/controllers/ClientRestController.java
@@ -3,12 +3,17 @@ package com.treviratech.clientsapibackend.controllers;
 import com.treviratech.clientsapibackend.models.entity.Client;
 import com.treviratech.clientsapibackend.models.services.IClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
+@CrossOrigin(origins = {"http://localhost:4200"})//Giving access to Angular app in port 4200 to this API
 @RestController
+@RequestMapping("/api") // This is optional
 public class ClientRestController {
 
     @Autowired
