0c306f4b0f7e610e3ab7e52fcc7fdd88c236e4e6
Juantrevi <juantrevi70@gmail.com>
2023-10-09 23:07:28 +0100

    ExceptionHandling Complete (Delete with Client client = clientService.findById(id);)

--- src/main/java/com/treviratech/clientsapibackend/controllers/ClientRestController.java
+++ src/main/java/com/treviratech/clientsapibackend/controllers/ClientRestController.java
@@ -19,13 +19,29 @@ public class ClientRestController {
     @Autowired
     private IClientService clientService;
 
+    @PostMapping("/clients")
+    public ResponseEntity<?> create(@RequestBody Client client) {
+        Client savedClient = null;
+        Map<String, Object> response = new HashMap<>();
+        try{
+            savedClient = clientService.save(client);
+        }catch (DataAccessException e){
+            response.put("message", "Error trying to insert into the DB" );
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        response.put("message", "Client created successfully");
+        response.put("client", savedClient);
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+    }
+
     @GetMapping("/clients")
-    public List<Client> index() {
+    public List<Client> readAll() {
         return clientService.findAll();
     }
 
     @GetMapping("/clients/{id}")
-    public ResponseEntity<?> show(@PathVariable Long id) {
+    public ResponseEntity<?> read(@PathVariable Long id) {
         Client client = null;
         Map<String, Object> response = new HashMap<>();
         try {
@@ -44,28 +60,6 @@ public class ClientRestController {
         }
 
         return new ResponseEntity<Client>(client, HttpStatus.OK);
-    }
-
-    @PostMapping("/clients")
-    public ResponseEntity<?> create(@RequestBody Client client) {
-        Client savedClient = null;
-        Map<String, Object> response = new HashMap<>();
-        try{
-            savedClient = clientService.save(client);
-        }catch (DataAccessException e){
-            response.put("message", "Error trying to insert into the DB" );
-            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
-            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        response.put("message", "Client created successfully");
-        response.put("client", savedClient);
-        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
-    }
-
-    @DeleteMapping("/clients/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id) {
-        clientService.delete(id);
     }
 
     @PutMapping("/clients/{id}")
@@ -100,4 +94,29 @@ public class ClientRestController {
         return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
     }
 
+    @DeleteMapping("/clients/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        Map<String, Object> response = new HashMap<>();
+        Client client = clientService.findById(id);
+
+        if (client == null) {
+            response.put("error", "Not Found");
+            response.put("message", "Client ID: ".concat(id.toString()).concat(" does not exist"));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+        try {
+            clientService.delete(id);
+            response.put("message", "Client deleted successfully");
+
+        }catch (DataAccessException e){
+            response.put("message", "Error trying to delete the client from the DB" );
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
+    }
+
 }
+
+
