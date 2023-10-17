673484cf0dc7e03759dc2fe7569d610c4bbe0410
Juantrevi <juantrevi70@gmail.com>
2023-10-17 10:49:27 +0100

    -- Figuring out oauth --

---
+++ src/main/java/com/treviratech/clientsapibackend/auth/AuthorisationServerConfig.java
@@ -0,0 +1,10 @@
+package com.treviratech.clientsapibackend.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+
+@Configuration
+public class AuthorisationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+
+}
--- src/main/java/com/treviratech/clientsapibackend/auth/SpringSecurityConfig.java
+++ src/main/java/com/treviratech/clientsapibackend/auth/SpringSecurityConfig.java
@@ -44,10 +44,15 @@ public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorize -> authorize
+                .authorizeHttpRequests
+                        (authorize -> authorize
                         .requestMatchers("/api/clients", "/api/clients/{id}", "/api/clients/page/{id}").permitAll()
-                        .anyRequest().authenticated()
-                );
+                        .anyRequest().authenticated());
+
+                        /*
+                        .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter()));
+                        */
+
         return http.build();
     }
 }
